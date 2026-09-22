2deae2f963c44fcbdc5ef43681239ef2209b9063
VishvaBhimani <vishvabhimani123@gmail.com>
2026-09-22 23:07:54 +0530

    data and run bat and task-5 added

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+title ClaimCheck AI - Task-5 Single Application
+echo ========================================================
+echo   Launching ClaimCheck AI (Task-5 ML Application)...
+echo ========================================================
+streamlit run app.py
+pause
